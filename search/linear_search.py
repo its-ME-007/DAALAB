ef863937981673677ae0181bcbaf1493ed24c18c
Anirudh <anirudh007kulkarni@gmail.com>
2025-06-17 01:27:43 +0530

    first commit on code runner

--- search/linear_search.py
+++
@@ -1,9 +0,0 @@
-def linear_search(arr: list, target: int) -> int:
-    """
-    Perform linear search on the given array.
-    Returns the index of the target if found, -1 otherwise.
-    """
-    for i in range(len(arr)):
-        if arr[i] == target:
-            return i
-    return -1 
